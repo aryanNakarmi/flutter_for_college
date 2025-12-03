b5c3572b0af4c90b9e3f6f6baf840f29734ea6d7
Aryan <aaryan.nakarmi111@gmail.com>
2025-12-03 10:55:54 +0545

    widget

--- lib/widgets/listview_widget.dart
+++ lib/widgets/listview_widget.dart
@@ -3,9 +3,35 @@ import 'package:flutter_for_college/models/student_model.dart';
 
 class ListviewWidget extends StatelessWidget {
   const ListviewWidget({super.key, required this.lstStudents});
+
   final List<StudentModel> lstStudents;
+
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return lstStudents.isNotEmpty?
+     ListView.builder(
+      shrinkWrap: true,
+      physics: const NeverScrollableScrollPhysics(),
+      itemBuilder: (context, index) {
+        final student = lstStudents[index];
+        return Card(
+          margin: const EdgeInsets.symmetric(vertical: 8, horizontal: 16),
+          child: ListTile(
+            leading: CircleAvatar(
+              child: Text(student.fname[0].toUpperCase()),
+            ),
+            title: Text('${student.fname} ${student.lname}'),
+            subtitle: Text(student.city),
+            trailing: Wrap(
+
+            ),
+          )
+        );
+      }
+     ):Center(
+      child: Text("No Data",
+      textAlign: TextAlign.center,
+      style: TextStyle(fontSize:24, color: Colors.grey.shade400),),
+     );
   }
 }
