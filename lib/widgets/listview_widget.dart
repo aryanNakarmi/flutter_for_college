49e0bd550c16a57504126ff106108a61f125da2b
Aryan <aaryan.nakarmi111@gmail.com>
2025-12-03 10:45:17 +0545

    widget

---
+++ lib/widgets/listview_widget.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class ListviewWidget extends StatelessWidget {
+  const ListviewWidget({super.key, required this.lstStudents});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
