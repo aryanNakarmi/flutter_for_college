7b53b611214ec7f561c1adde35a66f9a63128eec
Aryan <aaryan.nakarmi111@gmail.com>
2025-12-03 11:06:19 +0545

    output

--- lib/screens/output_screen.dart
+++ lib/screens/output_screen.dart
@@ -1,8 +1,9 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_for_college/models/student_model.dart';
 
 class OutputScreen extends StatelessWidget {
-  const OutputScreen({super.key});
-
+  const OutputScreen({super.key, required this.lstStudents});
+  final List<StudentModel> lstStudents;
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -10,9 +11,14 @@ class OutputScreen extends StatelessWidget {
         title: const Text('Output Screen'),
         backgroundColor: Theme.of(context).colorScheme.inversePrimary,
       ),
-      body: const Center(
+      body: lstStudents.isNotEmpty ?
+      ListView.builder(
+        ListviewWidget(lstStudents: _lstStudents,
+      ),
+      
         child: Text(
-          'output screen',
+          'No Data',
+          textAlign: TextAlign.center,
           style: TextStyle(fontSize: 24),
         ),
       ),
