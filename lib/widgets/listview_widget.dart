d4f080ebecaec98f961fc94a04cdf31728d0a5f6
Aryan <aaryan.nakarmi111@gmail.com>
2025-12-03 10:47:13 +0545

    widget

--- lib/widgets/listview_widget.dart
+++ lib/widgets/listview_widget.dart
@@ -1,8 +1,9 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_for_college/models/student_model.dart';
 
 class ListviewWidget extends StatelessWidget {
   const ListviewWidget({super.key, required this.lstStudents});
-
+  final List<StudentModel> lstStudents;
   @override
   Widget build(BuildContext context) {
     return const Placeholder();
