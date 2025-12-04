4f6b4926e57335e23c727129ee2a2ff062c7e68d
Aryan <aaryan.nakarmi111@gmail.com>
2025-12-04 12:56:53 +0545

    model

---
+++ lib/models/employee_model.dart
@@ -0,0 +1,15 @@
+class EmployeeModel {
+  final int id;
+  final String fname;
+  final String lname;
+  final String gender;
+  final String department;
+  
+  EmployeeModel({
+    required this.id,
+    required this.fname,
+    required this.lname,
+    required this.gender,
+    required this.department
+  });
+}
