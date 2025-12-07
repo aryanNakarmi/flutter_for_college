defa2fc00833dbc569e7406df3b3801aa90fc084
Aryan <aaryan.nakarmi111@gmail.com>
2025-12-07 12:25:58 +0545

    gridview

--- lib/screens/gridview_screen.dart
+++ lib/screens/gridview_screen.dart
@@ -10,9 +10,32 @@ class GridViewScreen extends StatelessWidget {
         title: const Text('GridView'),
         backgroundColor: Theme.of(context).colorScheme.inversePrimary,
       ),
-      body: const Center(
-        child: Text('gridview', style: TextStyle(fontSize: 24)),
-      ),
+      body: GridView.count(
+        crossAxisSpacing: 8,
+        mainAxisSpacing: 8,
+        crossAxisCount: 2,
+        children: [
+          Container(
+            alignment: Alignment.center,
+            color: Colors.amber,
+            child: const Text(
+              '1',
+              style: TextStyle(fontSize: 50),
+            ),
+            ),
+          
+          Container(
+            alignment: Alignment.center,
+            color: Colors.amber,
+            child: const Text(
+              '2',
+              style: TextStyle(fontSize: 50),
+            ),
+            ),
+          
+        ],
+
+      )
     );
   }
 }
