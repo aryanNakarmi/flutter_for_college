687e4c6dbe11f9c5bd8250eb60f20f74299f268c
Aryan <aaryan.nakarmi111@gmail.com>
2025-12-12 11:51:24 +0545

    theme data

--- lib/app.dart
+++ lib/app.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_for_college/screens/bottom_navigation_screen.dart';
 import 'package:flutter_for_college/screens/dashboard_screen.dart';
+import 'package:flutter_for_college/theme_data.dart';
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
@@ -10,7 +11,7 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       title: 'Flutter Apps for College',
       debugShowCheckedModeBanner: false,
-      
+      theme: getApplicationTheme(),
       home: const DashboardScreen(),
     );
   }
